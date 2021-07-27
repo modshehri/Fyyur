d864149c50ef53800e5aa86c1e57ebb15f936f8b
modshehri <mod.shehri@hotmail.com>
2021-07-27 07:20:47 +0300

    Implementing artists search route

--- app.py
+++ app.py
@@ -240,17 +240,25 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  search_term = request.values['search_term']
+  artists = db.session.query(Artist).filter(func.lower(Artist.name).contains(search_term.lower(), autoescape=True)).all()
+  
+  count = 0 
+  data = []
+  
+  for artist in artists:
+    a = {}
+    a["id"] = artist.id
+    a["name"] = artist.name
+    a["num_upcoming_shows"] = count
+    count += 1
+    data.append(a)
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": count,
+    "data": data
   }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
